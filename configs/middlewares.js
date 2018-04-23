112049187f7ff0512eff831f4280d9d11589ace3
seanng <shonum@gmail.com>
2018-04-23 16:22:22 +0800

    feat: add body-parser to parse image

--- configs/middlewares.js
+++ configs/middlewares.js
@@ -1,8 +1,10 @@
 const morgan = require('morgan');
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 module.exports = app => {
+  app.use(bodyParser.json({ limit: '50mb' }));
   app.use(morgan('dev'));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
